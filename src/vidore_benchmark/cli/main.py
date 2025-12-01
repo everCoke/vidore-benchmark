1ceef195eed74d1311cebb525b22d77b4c583787
everCoke <wuzhijun@mail.ustc.edu.cn>
2025-12-01 11:46:56 +0000

    modify

--- src/vidore_benchmark/cli/main.py
+++ src/vidore_benchmark/cli/main.py
@@ -64,6 +64,7 @@ def _get_metrics_from_vidore_evaluator(
         # Check if dataset_name is a local path
         if Path(dataset_name).exists():
             ds = cast(Dataset, load_from_disk(dataset_name))
+            ds = ds[splite]
         else:
             ds = cast(Dataset, load_dataset(dataset_name, split=split))
         vidore_evaluator = ViDoReEvaluatorQA(vision_retriever)
@@ -138,9 +139,9 @@ def evaluate_retriever(
         typer.Option(help="Dataset collection to use for evaluation. Can be a Hf collection id or a local dirpath."),
     ] = None,
     split: Annotated[str, typer.Option(help="Dataset split")] = "test",
-    batch_query: Annotated[int, typer.Option(help="Batch size for query embedding inference")] = 4,
-    batch_passage: Annotated[int, typer.Option(help="Batch size for passages embedding inference")] = 4,
-    batch_score: Annotated[Optional[int], typer.Option(help="Batch size for retrieval score computation")] = 4,
+    batch_query: Annotated[int, typer.Option(help="Batch size for query embedding inference")] = 8,
+    batch_passage: Annotated[int, typer.Option(help="Batch size for passages embedding inference")] = 8,
+    batch_score: Annotated[Optional[int], typer.Option(help="Batch size for retrieval score computation")] = 8,
     dataloader_prebatch_query: Annotated[
         Optional[int], typer.Option(help="Dataloader prebatch size for queries")
     ] = None,
